80a4143f9758dca5fdceda0035eadecc43f60650
VitaliKozhurov <110495939+VitaliKozhurov@users.noreply.github.com>
2023-02-22 00:17:08 +0300

    fix double request

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from 'axios';
 import qs from 'qs';
@@ -10,6 +10,8 @@ import { useNavigate } from "react-router-dom";
 
 const Home = () => {
    const navigate = useNavigate();
+   const isGetRequest = useRef(false); // Создаем переменную, чтобы определять надо ли делать дефолтный запрос на сервер
+   const isMounted = useRef(false); // Создаем переменную, которая будет определять первый раз смонтирована страница или нет
 
    // Контекст
    const { searchState } = useContext(SearchContext);
@@ -26,39 +28,49 @@ const Home = () => {
    const [isLoaded, setLoad] = useState(true);
    const search = searchState ? `&search=${searchState}` : '';
 
-   // Запросы на сервер
    useEffect(() => {
-      setLoad(true);
-      // параметр sortBy, определяет сортировку, order - порядок, asc(возрастание), desc(убывание)
-      axios.get(`https://63f0f6655b7cf4107e2a2f99.mockapi.io/items?page=${currentPage}&limit=4&${categoryId ? `category=${categoryId}` : ''}&sortBy=${sortQuery[0]}&order=${sortQuery[1]}${search}`)
-         .then(({ data }) => {
-            setPizzas(data);
-            setLoad(false);
-         })
-
-      window.scrollTo(0, 0);// При возврате на главную страницу будем скролить ее вверх
-   }, [categoryId, sortType, searchState, currentPage]);
+      if (isMounted.current) {// Формируем строку запроса
+         const queryString = qs.stringify({
+            sortBy: sortType,
+            categoryId: categoryId,
+            currentPage: currentPage
+         });
+         // Navigate отображает в адресной строке нашу сформированную строку
+         navigate(`?${queryString}`);
+      }
+      isMounted.current = true; // Указываем что первый рендер произошел
+   }, [categoryId, sortType, currentPage])
 
    useEffect(() => {
+      // В случае если имеется значение в адресной строке после знака ?
+      // то эти параметры диспатчим в стор редакса
       if (window.location.search) {
          const params = qs.parse(window.location.search.substring(1));
          // Сохраняем в redux параметры из строки
          dispatch(
             setFilters(params)
          )
+         isGetRequest.current = true;
       }
    }, [])
 
+   // Запросы на сервер
    useEffect(() => {
-      // Формируем строку запроса
-      const queryString = qs.stringify({
-         sortBy: sortType,
-         categoryId: categoryId,
-         currentPage: currentPage
-      });
-      // Navigate отображает в адресной строке нашу сформированную строку
-      navigate(`?${queryString}`);
-   }, [categoryId, sortType, currentPage])
+      setLoad(true);
+      // При первой загрузки страницы если было какое-то значение в адресной строке сначала эти значения закидываем  в стор, а со второй и когда эти значения поменяются в сторе мы будем  почылать запрос
+      if (!isGetRequest.current) {
+         axios.get(`https://63f0f6655b7cf4107e2a2f99.mockapi.io/items?page=${currentPage}&limit=4&${categoryId ? `category=${categoryId}` : ''}&sortBy=${sortQuery[0]}&order=${sortQuery[1]}${search}`)
+            .then(({ data }) => {
+               setPizzas(data);
+               setLoad(false);
+            })
+      }
+
+      isGetRequest.current = false;
+
+      // параметр sortBy, определяет сортировку, order - порядок, asc(возрастание), desc(убывание)
+      window.scrollTo(0, 0); // При возврате на главную страницу будем скролить ее вверх
+   }, [categoryId, sortType, searchState, currentPage]);
 
 
    const skeletons = [...new Array(10)].map((el, ind) => <Loader key={ind} />);
